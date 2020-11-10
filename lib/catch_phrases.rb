9c85eaf0cc7ce2b30ea0404b142d261395f6e83c
Raz Al-Jaf <razaljaf@gmail.com>
2020-11-10 01:59:31 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -11,7 +11,7 @@ def link
   puts link
 end
 def all_phrases(phrase="It's-a me, Mario!", status='Thank You Mario! But Our Princess Is In Another Castle!', link="It's Dangerous To Go Alone! Take This.")
-  puts "#{phrase}, #{status}, #{link}"
+  puts "#{phrase}"
 end
 
 
